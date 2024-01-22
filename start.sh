af5cdbbb5de0039696974514b1590572ea59ea11
NG ROCKERS <77508122+NGROCKERS@users.noreply.github.com>
2024-01-22 11:41:22 +0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,13 @@
 ####
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/NGROCKERS/VJ-FILTER-BOT.git /VJ-FILTER-BOT 
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /VJ-FILTER-BOT 
+fi
+cd /VJ-FILTER-BOT 
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
